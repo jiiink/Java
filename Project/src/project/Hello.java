f59b3f63096bdbf994de73a39d1957fb855259ec
jiiink <bhkim0404@gmail.com>
2023-09-04 21:54:38 +0900

    .

--- Project/src/project/Hello.java
+++ Project/src/project/Hello.java
@@ -1,10 +1,10 @@
-package Project;
+// package project;
 
-public class Project {
+public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("eclipse");
+		System.out.println("hello");
 	}
 
 }
---
+++ Project/src/project/Test.java
@@ -0,0 +1,7 @@
+// package src.project;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+}
