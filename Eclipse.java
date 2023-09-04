83e4999355de6b71d1d02958606fc9ba415a9893
jiiink <bhkim0404@gmail.com>
2023-09-04 20:13:52 +0900

    ec

---
+++ Eclipse.java
@@ -0,0 +1,5 @@
+public class Eclipse {
+	public static void main(String[] args) {
+		System.out.println("Hello, eclipse");
+	}
+}
---
+++ Project/src/project/Project.java
@@ -0,0 +1,8 @@
+package project;
+
+public class Project {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("project");
+	}
+}
