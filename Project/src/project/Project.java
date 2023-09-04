3f8c6dc43bd2a5df3c57248aefe6c21fd48309bd
jiiink <bhkim0404@gmail.com>
2023-09-04 20:23:49 +0900

    eclipse

--- Project/src/project/Project.java
+++ Project/src/project/Project.java
@@ -1,8 +1,10 @@
-package project;
+package Project;
 
 public class Project {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("project");
+		System.out.println("eclipse");
 	}
+
 }
