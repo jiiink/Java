a57b9903eb0e99d3cd760253384076cc04d3b61e
김병현 <kimbyeonghyeon@myBook.local>
2023-09-20 20:46:37 +0900

    .

--- PBP/src/hw03/ArrayEnum.java
+++ PBP/src/hw03/ArrayEnum.java
@@ -1,20 +1,20 @@
-// package hw03;
+package hw03;
 import java.util.Scanner;
 
 enum Command {
 	// Your code here
-//	String command = "";
-	ADD("ADD"), LIST("LIST"), SUM("SUM"), QUIT("QUIT"), INVALID("INVALID");
-	private final String command;
-	private Command(final String command) {
-		this.command = command;
-	}
-}
+    ADD("ADD"), LIST("LIST"), SUM("SUM"), QUIT("QUIT"), INVALID("INVALID");
+    private final String command;
+    Command(final String command) {
+        this.command = command;
+    }
+};
 public class ArrayEnum {
 	public static void main(String[] args) {
 		// Your code here
-		int[] values = new int[100];
-		int index = 0;
+        final int[] values = new int[100];
+        int index = 0;
+
 		final Scanner scanner = new Scanner(System.in);
 		while ( true ) {
 			final Command command = getCommand(scanner); // Command is enum
@@ -41,34 +41,30 @@ public class ArrayEnum {
 		}
 		scanner.close();
 	}
-	private static Command getCommand(Scanner scanner) {
-		String str = scanner.next();
-		str = str.toUpperCase();
-	
-		if (!str.equals("ADD") && !str.equals("LIST") && !str.equals("SUM") && !str.equals("QUIT")) {
-			str = "INVALID";
-		}
-
-		Command command = Command.valueOf(str);
-		return command;
-	}
-	private static int getValue(Scanner scanner) {
-		int value = scanner.nextInt();
-		
-		return value;
-	}
-	private static void printList(int[] values, int index) {
-		for (int i=0; i<index; i++) {
-			System.out.printf("%d ", values[i]);
-		}
-		System.out.println();
-	}
-	private static int getSum(int[] values, int index) {
-		int sum = 0;
-		for (int i=0; i<=index; i++) {
-			sum += values[i];
-		}
-		return sum;
-	}
 	// Your code here
+    private static final Command getCommand(final Scanner scanner) {
+        String cmd = scanner.next().toString().toUpperCase();
+        if (!cmd.equals("ADD") && !cmd.equals("LIST") && !cmd.equals("SUM") && !cmd.equals("QUIT")) {
+            cmd = "INVALID";
+        }
+        final Command command = Command.valueOf(cmd);
+        return command;
+    }
+    private static final int getValue(final Scanner scanner) {
+        final int value = scanner.nextInt();
+        return value;
+    }
+    private static void printList(final int[] values, final int index) {
+        for (int i=0; i<index; i++) {
+            System.out.printf("%d ", values[i]);
+        }
+        System.out.println();
+    }
+    private static final int getSum(final int[] values, final int index) {
+        int sum = 0;
+        for (int i=0; i<index; i++) {
+            sum += values[i];
+        }
+        return sum;
+    }
 }
