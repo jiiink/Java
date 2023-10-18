9d316299735f4382749e5fb26593543a40ea4ee8
jiiink <bhkim0404@gmail.com>
2023-10-19 01:02:19 +0900

    .

--- PBP/src/hw08/Rectangle.java
+++ PBP/src/hw08/Rectangle.java
@@ -2,9 +2,15 @@ package hw08;
 // import hw08.Shape;
 
 public class Rectangle extends Shape {
-    private int area;
+    // private int area;
     public Rectangle(int w, int h) {
         super(w, h);
-        area = w * h;
+        // area = w * h;
+        // setArea(w, h);
+    }
+    @Override
+    public void setArea() {
+        // this.area = w * h;
+        area = width * height;
     }
 }
--- PBP/src/hw08/Shape.java
+++ PBP/src/hw08/Shape.java
@@ -5,15 +5,17 @@ import java.lang.Math;
 public abstract class Shape {
     private int width;
     private int height;
-    // private double area;
+    private double area;
 
     public Shape() {
         width = 0;
         height = 0;
     }
-
     public Shape(int w, int h) {
         this.width = w;
         this.height = h;
+        setArea();
+        // this.area = w * h;
     }
+    public abstract void setArea();
 }
